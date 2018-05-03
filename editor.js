b290f3b0878ac4641144357a229597034384ecaa
Austin Heiman <atheimanksu@gmail.com>
2018-05-03 10:51:17 -0500

    multi-file in progress

--- editor.js
+++ editor.js
@@ -1,26 +1,38 @@
-var editor = document.getElementById('editor')
+var editorTextarea = document.getElementById('editor')
+var filesSelect = document.getElementById('files')
+
+function correctTextareaSize() {
+  editorTextarea.style.height = "";
+  editorTextarea.style.height = editorTextarea.scrollHeight + "px";
+}
+
+function selectChange() {
+  // limit the selected options to 1
+}
 
 function pageLoad() {
-  var loaded = JSON.parse(localStorage.getItem('file1.txt'))
+  correctTextareaSize();
+
+  var loaded = JSON.parse(localStorage.getItem('file1.txt'));
 
   if (!loaded) {
-    editor.value = "Welcome to your lightweight, in-browser text editor.\nYour text is saved to your browser's localStorage and will be here when you return.";
-    textChange()
+    editorTextarea.value = "Welcome to your lightweight, in-browser text editor.\nYour text is saved to your browser's localStorage and will be here when you return.";
+    textChange();
     return;
   }
 
-  editor.value = loaded['value'];
-  editor.selectionStart = loaded['selectionStart'];
-  editor.selectionEnd = loaded['selectionEnd'];
+  editorTextarea.value = loaded['value'];
+  editorTextarea.selectionStart = loaded['selectionStart'];
+  editorTextarea.selectionEnd = loaded['selectionEnd'];
 }
 
 function textChange() {
   localStorage.setItem(
     'file1.txt',
     JSON.stringify({
-      value: editor.value,
-      selectionStart: editor.selectionStart,
-      selectionEnd: editor.selectionEnd,
+      value: editorTextarea.value,
+      selectionStart: editorTextarea.selectionStart,
+      selectionEnd: editorTextarea.selectionEnd,
     })
   );
 }
